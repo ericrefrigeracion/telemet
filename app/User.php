5549444816e7cb7f875ee7aae5b93a30de3066a7
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-06-23 20:03:32 -0300

    debug @cgavernet

--- app/User.php
+++ app/User.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Caffeinated\Shinobi\Concerns\HasRolesAndPermissions;
 
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable, HasRolesAndPermissions;
 
