e39711287abb6bf0cf49db12277c8d0e258b2b66
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-07-05 11:55:58 -0300

    debug @cgavernet

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -20,6 +20,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'http://www.sysnet.com.ar/webhooks',
+        '/webhooks',
         'http://telemett.test/webhooks'
     ];
 }
