9b8e370c2e674f791e162999ab78bd716371af68
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-07-05 19:05:14 -0300

    debug @cgavernet

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -26,16 +26,16 @@ class WebhookController extends Controller
     public function store(Request $request)
     {
 
-        $webhook = new Webhook;
-        $webhook->id = $request->input('id');
-        $webhook->type = $request->input('type');
-        $webhook->user_id = $request->input('user_id');
-        $webhook->application_id = $request->input('application_id');
-        $webhook->version = $request->input('version');
-        $webhook->action = $request->input('action');
-        $webhook->data = $request->input('data.id');
-        $webhook->date_created = $request->input('date_created');
-        $webhook->save();
+        //$webhook = new Webhook;
+        //$webhook->id = $request->input('id');
+        //$webhook->type = $request->input('type');
+        //$webhook->user_id = $request->input('user_id');
+        //$webhook->application_id = $request->input('application_id');
+        //$webhook->version = $request->input('version');
+        //$webhook->action = $request->input('action');
+        //$webhook->data = $request->input('data.id');
+        //$webhook->date_created = $request->input('date_created');
+        //$webhook->save();
 
         return response('Objeto creado', 201);
     }
