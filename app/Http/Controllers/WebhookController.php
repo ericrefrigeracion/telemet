14a5ac6c31f8477a2c4c741677fa46022fdc8d01
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-09-03 17:17:54 -0300

    debugs

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -62,7 +62,7 @@ class WebhookController extends Controller
     {
 
         $payment_id = $request->data_id;
-        echo ' -' . $payment_id . ' u-' . $user_id . ' d-' . $device_id . ' p-' . $price_id . ' ret-';
+
         PaymentRevissionJob::dispatch($payment_id, $user_id, $device_id, $price_id);
 
         return 201;
