829689daee960f5ecbf08e9443d089b444411e2a
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-09-06 17:40:43 -0300

    devices.index

--- resources/views/devices/index.blade.php
+++ resources/views/devices/index.blade.php
@@ -13,9 +13,7 @@
                         </div>
                         <div class="card-body">
                             <div class="row">
-                                <div class="col">
-                                    <h6 class="card-subtitle text-muted">{{ $device->description }}</h6>
-                                </div>
+                                <h6 class="col card-subtitle text-muted m-0">{{ $device->description }}</h6>
                             </div>
                             <div class="row">
                                  <div class="col text-center m-2">
@@ -43,46 +41,41 @@
                             <div class="row">
                                 @if($device->admin_mon)
                                     <div class="col-2">
-                                        <i class="fas fa-user-shield text-success mt-3" title="Monitoreo Activo"></i>
+                                        <i class="fas fa-user-shield text-success m-2" title="Monitoreo Activo"></i>
                                         @if($device->tmon)
-                                            <i class="fas fa-temperature-high text-success mt-3" title="Control de Temperatura Activo"></i>
+                                            <i class="fas fa-temperature-high text-success m-2" title="Control de Temperatura Activo"></i>
                                         @else
-                                            <i class="fas fa-temperature-high text-danger mt-3" title="Control de Temperatura Desactivado"></i>
+                                            <i class="fas fa-temperature-high text-danger m-2" title="Control de Temperatura Desactivado"></i>
                                         @endif
                                         @if($device->rule_type == 'Siempre Protegido')
-                                            <i class="far fa-check-square text-success mt-3" title="Siempre Protegido"></i>
+                                            <i class="far fa-check-square text-success m-2" title="Siempre Protegido"></i>
                                         @endif
                                         @if($device->rule_type == 'Protegido cuando cierro mi Comercio')
-                                            <i class="fas fa-cash-register text-success mt-3" title="Protegido cuando cierro mi Comercio"></i>
+                                            <i class="fas fa-cash-register text-success m-2" title="Protegido cuando cierro mi Comercio"></i>
                                         @endif
                                         @if($device->rule_type == 'Con horarios Permitidos (Perzonalizado)')
-                                            <a href="{{ route('rules.index') }}"><i class="far fa-clock text-success mt-3" title="Con horarios Permitidos (Perzonalizado)"></i></a>
+                                            <a href="{{ route('rules.index') }}"><i class="far fa-clock text-success m-2" title="Con horarios Permitidos (Perzonalizado)"></i></a>
                                         @endif
                                     </div>
                                 @endif
-                                <div class="col-10">
-                                    <div class="row">
-
-                                    </div>
-                                    <div class="row">
-                                        <div class="col text-center">
-                                            <h4 class="card-title display-4 m-2">{{ $device->last_data01 }}°C</h4>
-                                        </div>
-                                    </div>
+                                <div class="col-10 text-center">
+                                    <h4 class="card-title display-4 m-2">{{ $device->last_data01 }}°C</h4>
                                 </div>
                             </div>
 
 
                             <div class="row">
-                                <div class="col h4 text-center m-2">
+                                <div class="col h4 text-center m-0 mt-3">
                                     @can('receptions.show-hour')
                                         <a href="{{ route('receptions.show-hour', $device->id) }}" class="text-primary m-2" title="Graficos De Temperatura"><i class="fas fa-chart-line"></i></a>
                                     @endcan
                                     @can('devices.show')
                                         <a href="{{ route('devices.show', $device->id) }}" class="text-primary m-2" title="Configuracion Del Dispositivo"><i class="fas fa-cogs"></i></a>
                                     @endcan
-                                    @can('devices.show')
-                                        <a href="{{ route('alerts.show', $device->id) }}" class="text-primary m-2" title="Nuevas Alertas">{{ $device->alerts_count }} <i class="fas fa-bell"></i></a>
+                                    @can('alerts.show')
+                                        @if($device->alerts_count > 0)
+                                            <a href="{{ route('alerts.show', $device->id) }}" class="text-primary m-2" title="Nuevas Alertas">{{ $device->alerts_count }} <i class="fas fa-bell"></i></a>
+                                        @endif
                                     @endcan
                                 </div>
                             </div>
