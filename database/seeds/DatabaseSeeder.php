d23dd32cc5ca57c18053670a7328f8f76fb3d6ab
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-09-10 23:34:45 -0300

    type devices table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(TypeRulesTableSeeder::class);
-        $this->call(TypeDevicesTableSeeder::class);
+        //$this->call(TypeRulesTableSeeder::class);
+        //$this->call(TypeDevicesTableSeeder::class);
         $this->call(PricesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(UserPermissionsTableSeeder::class);
