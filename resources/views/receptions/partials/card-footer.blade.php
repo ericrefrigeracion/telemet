fdeb1ebe7e846f1f754bb0d92f865f236fd3c96e
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-10-22 18:42:30 -0300

    debugs

--- resources/views/receptions/partials/card-footer.blade.php
+++ resources/views/receptions/partials/card-footer.blade.php
@@ -1,7 +1,7 @@
-@can('receptions.show-hour')
-    <a href="{{ route('receptions.show-hour', $device->id) }}" class="btn btn-sm btn-primary m-1">Ver ultima hora</a>
-@endcan
 @if($device->admin_mon)
+    @can('receptions.show-hour')
+        <a href="{{ route('receptions.show-hour', $device->id) }}" class="btn btn-sm btn-primary m-1">Ver ultima hora</a>
+    @endcan
     @can('receptions.show-day')
         <a href="{{ route('receptions.show-day', $device->id) }}" class="btn btn-sm btn-primary m-1">Ver ultimo dia</a>
     @endcan
