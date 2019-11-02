af584f6f96dea0388ec68ddd7eeb72b3da43b320
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-11-02 09:18:14 -0300

    debugs

--- resources/views/receptions/partials/plot.blade.php
+++ resources/views/receptions/partials/plot.blade.php
@@ -21,6 +21,30 @@ $.getJSON(
                 enabled: true
             },
             plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
             },
 
             series: [{
