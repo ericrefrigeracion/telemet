fca8cc416e7912a693951b70ed2ddf72b17a0d12
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-11-02 09:28:04 -0300

    plot debugs

--- resources/views/receptions/partials/plot.blade.php
+++ resources/views/receptions/partials/plot.blade.php
@@ -1,6 +1,6 @@
 $.getJSON(
-    '',
-    function () {
+    'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/usdeur.json',
+    function (data) {
 
         Highcharts.chart('plot', {
             chart: {
@@ -21,30 +21,6 @@ $.getJSON(
                 enabled: true
             },
             plotOptions: {
-                area: {
-                    fillColor: {
-                        linearGradient: {
-                            x1: 0,
-                            y1: 0,
-                            x2: 0,
-                            y2: 1
-                        },
-                        stops: [
-                            [0, Highcharts.getOptions().colors[0]],
-                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                        ]
-                    },
-                    marker: {
-                        radius: 2
-                    },
-                    lineWidth: 1,
-                    states: {
-                        hover: {
-                            lineWidth: 1
-                        }
-                    },
-                    threshold: null
-                }
             },
 
             series: [{
