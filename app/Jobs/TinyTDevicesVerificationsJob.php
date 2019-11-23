a8dffa05671d2c206bc0fd315debc89b4d26af0d
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-11-23 16:05:53 -0300

    debugs

--- app/Jobs/TinyTDevicesVerificationsJob.php
+++ app/Jobs/TinyTDevicesVerificationsJob.php
@@ -189,7 +189,7 @@ class TinyTDevicesVerificationsJob implements ShouldQueue
         if($derivate < -10) $derivate = -10;
 
         $status = 0;
-        $derivate_avg = $device->receptions()->where('created_at', '>', $time)->avg('data08');
+        $derivate_avg = $device->receptions()->where('created_at', '>', $status_time)->avg('data08');
         if($derivate_avg >= 0) $status = 1;
         if($derivate_avg < 0) $status = 0;
 
