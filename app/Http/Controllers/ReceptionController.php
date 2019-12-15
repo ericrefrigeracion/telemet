b9cc37a35e2b239dd2189430aef21ab9ee739307
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-12-15 09:54:50 -0300

    debugs

--- app/Http/Controllers/ReceptionController.php
+++ app/Http/Controllers/ReceptionController.php
@@ -30,7 +30,12 @@ class ReceptionController extends Controller
             if ($datas->isNotEmpty())
             {
 
-                foreach ($datas as $data) $data->created_at_unix = ($data->created_at->timestamp - (3 * 60 * 60)) * 1000;
+                foreach ($datas as $data)
+                {
+                    $data->created_at_unix = ($data->created_at->timestamp - (3 * 60 * 60)) * 1000;
+                    $data->data01 = $data->data01 + $device->tiny_t_device()->tcal;
+                    $data->data02 = $data->data02 + $device->tiny_t_device()->tcal;
+                }
 
                 return view('receptions.now')->with(['device' => $device, 'datas' => $datas]);
 
