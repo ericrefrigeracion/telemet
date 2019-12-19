6c1b4e8a3f603e208181a597a2b9c2855781ca5d
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-12-19 07:22:15 -0300

    debugs

--- resources/views/email/centinela/admin/temp_perfromance.blade.php
+++ resources/views/email/centinela/admin/temp_perfromance.blade.php
@@ -2,9 +2,7 @@
 #Sr. Administrador:
 # {{ $device->id }} - {{ $device->name }} tiene un rendimiento por debajo del esperado.
 
-El equipo del sr {{ $user->name }} {{ $user->surname }} con ID {{ $device->id }} se encuentra fuera de set point al no alcanzar el valor de temperatura deseado en el tiempo previsto en el retardo para el aviso, en la configuracion del dispositivo.
-
-La ultima vez que el equipo llego a la temperatura deseada fue {{ $mail_information->last_created_at }}.
+El equipo del sr {{ $user->name }} {{ $user->surname }} con ID {{ $device->id }} se encuentra con bajo rendimiento, esto quiere decir que no ha bajado los grados centigrados por hora esperados. Este valor puede modificarse en la configuracion del dispositivo.
 
 <hr>
 El numero de telefono del usuario es {{ $user->phone_area_code }} - {{ $user->phone_number }}, su direccion es {{ $user->address }}.
