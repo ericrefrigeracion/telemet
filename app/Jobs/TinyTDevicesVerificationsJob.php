0e96dfe9b2efc13485ec1bb136f85bf383e8c995
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2019-12-19 21:28:12 -0300

    debugs

--- app/Jobs/TinyTDevicesVerificationsJob.php
+++ app/Jobs/TinyTDevicesVerificationsJob.php
@@ -64,7 +64,7 @@ class TinyTDevicesVerificationsJob implements ShouldQueue
     {
         $product_time = now()->subHours(7);
         $cooling_time= now()->subHours(3);
-        $status_time = now()->subMinutes(6);
+        $status_time = now()->subMinutes(8);
 
         foreach ($devices as $device)
         {
