c288a7a8b3e7073985a6c95e051caa4834ec1843
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2020-01-29 13:57:45 -0300

    debug jobs

--- app/Jobs/TinyTDevicesVerificationsJob.php
+++ app/Jobs/TinyTDevicesVerificationsJob.php
@@ -182,7 +182,7 @@ class TinyTDevicesVerificationsJob implements ShouldQueue
 
     public function productTemperature($device, $last_reception, $before_reception, $cooling_time, $status_time)
     {
-        dd($device);
+        dd($last_reception);
         $derivate = $before_reception->data01 - $last_reception->data01;
         if($derivate > 10) $derivate = 10;
         if($derivate < -10) $derivate = -10;
