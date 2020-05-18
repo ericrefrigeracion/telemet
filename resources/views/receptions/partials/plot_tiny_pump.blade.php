0c5731a7e194955fd6ed6b45a2ac92345fcf368b
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2020-05-18 00:08:34 -0300

    debug plots

--- resources/views/receptions/partials/plot_tiny_pump.blade.php
+++ resources/views/receptions/partials/plot_tiny_pump.blade.php
@@ -18,8 +18,8 @@ $.getJSON(
                     text: null
                 },
                 plotBands: [ {
-                    from: {{ $device->tiny_t_device->tmin }},
-                    to: {{ $device->tiny_t_device->tmax }},
+                    from: {{ $device->tiny_pump_device->l_min }},
+                    to: {{ $device->tiny_pump_device->l_max }},
                     color: 'rgba(68, 170, 213, 0.1)',
                     label: {
                         text: null,
@@ -40,7 +40,7 @@ $.getJSON(
                 data: [
                         @foreach($datas as $data)
                             @if(isset($data->data01))
-                            [ {{ ($data->created_at->timestamp - (3 * 60 * 60)) * 1000 }}, {{ $data->data01 + $device->tiny_t_device->tcal }} ],
+                            [ {{ ($data->created_at->timestamp - (3 * 60 * 60)) * 1000 }}, {{ $data->data01 + $device->tiny_pump_device->l_cal }} ],
                             @endif
                         @endforeach
                 ],
@@ -52,17 +52,113 @@ $.getJSON(
                     enabled: false
                 }
             },{
-                name: 'Temperatura estimada del producto',
+                name: '{{ $device->type_device->data02_name }}',
                 data: [
                         @foreach($datas as $data)
                             @if(isset($data->data02))
-                            [ {{ ($data->created_at->timestamp - (3 * 60 * 60)) * 1000 }}, {{ $data->data02 + $device->tiny_t_device->tcal }} ],
+                            [ {{ ($data->created_at->timestamp - (3 * 60 * 60)) * 1000 }}, {{ $data->data02 }} ],
                             @endif
                         @endforeach
                 ],
                 tooltip: {
                     valueDecimals: 2,
-                    valueSuffix: ' {{ $device->type_device->data01_unit }}'
+                    valueSuffix: ' {{ $device->type_device->data02_unit }}'
+                },
+                marker: {
+                    enabled: false
+                }
+            },{
+                name: '{{ $device->type_device->data03_name }}',
+                data: [
+                        @foreach($datas as $data)
+                            @if(isset($data->data03))
+                            [ {{ ($data->created_at->timestamp - (3 * 60 * 60)) * 1000 }}, {{ $data->data03 + 2 }} ],
+                            @endif
+                        @endforeach
+                ],
+                tooltip: {
+                    valueDecimals: 2,
+                    valueSuffix: ' {{ $device->type_device->data03_unit }}'
+                },
+                marker: {
+                    enabled: false
+                }
+            },{
+                name: '{{ $device->type_device->data04_name }}',
+                data: [
+                        @foreach($datas as $data)
+                            @if(isset($data->data04))
+                            [ {{ ($data->created_at->timestamp - (3 * 60 * 60)) * 1000 }}, {{ $data->data04 + 4 }} ],
+                            @endif
+                        @endforeach
+                ],
+                tooltip: {
+                    valueDecimals: 2,
+                    valueSuffix: ' {{ $device->type_device->data04_unit }}'
+                },
+                marker: {
+                    enabled: false
+                }
+            },{
+                name: '{{ $device->type_device->data05_name }}',
+                data: [
+                        @foreach($datas as $data)
+                            @if(isset($data->data05))
+                            [ {{ ($data->created_at->timestamp - (3 * 60 * 60)) * 1000 }}, {{ $data->data05 + 6}} ],
+                            @endif
+                        @endforeach
+                ],
+                tooltip: {
+                    valueDecimals: 2,
+                    valueSuffix: ' {{ $device->type_device->data05_unit }}'
+                },
+                marker: {
+                    enabled: false
+                }
+            },{
+                name: '{{ $device->type_device->data06_name }}',
+                data: [
+                        @foreach($datas as $data)
+                            @if(isset($data->data06))
+                            [ {{ ($data->created_at->timestamp - (3 * 60 * 60)) * 1000 }}, {{ $data->data06 + 8}} ],
+                            @endif
+                        @endforeach
+                ],
+                tooltip: {
+                    valueDecimals: 2,
+                    valueSuffix: ' {{ $device->type_device->data06_unit }}'
+                },
+                marker: {
+                    enabled: false
+                }
+            },{
+                name: '{{ $device->type_device->data07_name }}',
+                data: [
+                        @foreach($datas as $data)
+                            @if(isset($data->data07))
+                            [ {{ ($data->created_at->timestamp - (3 * 60 * 60)) * 1000 }}, {{ $data->data07 + 10}} ],
+                            @endif
+                        @endforeach
+                ],
+                tooltip: {
+                    valueDecimals: 2,
+                    valueSuffix: ' {{ $device->type_device->data07_unit }}'
+                },
+                marker: {
+                    enabled: false
+                }
+            },{
+                name: '{{ $device->type_device->data08_name }}',
+                data: [
+                        @foreach($datas as $data)
+                            @if(isset($data->data08))
+                            [ {{ ($data->created_at->timestamp - (3 * 60 * 60)) * 1000 }}, {{ $data->data08 }} ],
+                            @endif
+                        @endforeach
+                ],
+                tooltip: {
+                    valueDecimals: 2,
+                    valueSuffix: ' {{ $device->type_device->data08_unit }}'
                 },
                 marker: {
                     enabled: false
