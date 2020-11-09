9f81844a3e522acde8e3d0ed3b65dfce819c2e5a
ericrefrigeracion <ericlopezrefrigeracion@hotmail.com>
2020-11-09 01:45:21 -0300

    debug impersonate

--- resources/views/layouts/partials/errors.blade.php
+++ resources/views/layouts/partials/errors.blade.php
@@ -36,7 +36,7 @@
                     <div class="col-md-10">
                         <div class="alert alert-success">
                             <button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            Usted se encuentra navegando como el usuario {{ auth()->user()->name }}. Click <a href="{{ route('impersonate.stop') }}">aqui</a> para recuperar su sesion.
+                            Usted se encuentra navegando como el usuario {{ auth()->user()->name }}. Click <a href="{{ route('impersonate.stop') }}">aqui</a> para recuperar su sesion original.
                         </div>
                     </div>
                 </div>
